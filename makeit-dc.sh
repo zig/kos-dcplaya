4d24b397124aefcb4166b3fd4e6204a83b545019
Vincent Penne <ziggy@sashipa.com>
2009-07-27 01:46:38 +0200

    try to fix the brain dead kos build process

--- makeit-dc.sh
+++ makeit-dc.sh
@@ -9,4 +9,4 @@ export DC_ARM_LD="${DC_ARM_BASE}/bin/${DC_ARM_PREFIX}-ld"
 export DC_ARM_CFLAGS="-mcpu=arm7 -Wall -O2"
 export DC_ARM_AFLAGS="-marm7"
 
-make KOS_CFLAGS="$KOS_CFLAGS -I$KOS_BASE/libc/include -I$KOS_BASE/libm/common -I$KOS_BASE/include/newlib-libm-sh4 -I$KOS_BASE/kernel/arch/$KOS_ARCH/include -I$KOS_BASE/include -I$KOS_BASE/addons/include -I$KOS_BASE/addons/lwip/lwip/src/include -I$KOS_BASE/addons/lwip/kos/include -I$KOS_BASE/addons/lwip/lwip/src/include/ipv4 -D_arch_dreamcast" $@
+make KOS_CFLAGS="$KOS_CFLAGS -I$KOS_BASE/libc/include -I$KOS_BASE/libm/common -I$KOS_BASE/include/newlib-libm-sh4 -I$KOS_BASE/kernel/arch/$KOS_ARCH/include -I$KOS_BASE/include -I$KOS_BASE/addons/include -I$KOS_BASE/addons/lwip/lwip/src/include -I$KOS_BASE/addons/lwip/kos/include -I$KOS_BASE/addons/lwip/lwip/src/include/ipv4 -I$KOS_BASE/addons/liboggvorbis/liboggvorbis/libvorbis/include -I$KOS_BASE/addons/liboggvorbis/liboggvorbis/libvorbis/lib -D_arch_dreamcast" $@
