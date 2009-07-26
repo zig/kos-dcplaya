f5b3a1fedb4dd46ed682e301a0cd9f932cfdc3e9
Vincent Penne <ziggy@sashipa.com>
2009-07-27 01:55:01 +0200

    added more missing addons for dcplaya

---
+++ addons/libkosh/builtin.h
@@ -0,0 +1,17 @@
+/*
+ * builtin.h - shell builtins
+ *
+ * (C) 2000 Jordan DeLong
+ */
+#ifndef _KOSH_BUILTIN_H
+#define _KOSH_BUILTIN_H
+
+/* the func */
+int builtin_command(int argc, char *argv[]);
+
+void builtins_init();
+void builtins_shutdown();
+
+#endif
+		
+
---
+++ addons/libkosh/kosh.c
@@ -0,0 +1,47 @@
+/*
+ * kosh.c - console shell for kallistios (A Rough version)
+ *
+ * (C) 2000 Jordan DeLong
+ */
+
+#include <stdio.h>
+#include "kosh.h"
+#include "chdir.h"
+#include "builtin.h"
+
+/* global exit flag:  if someone sets this we quit */
+volatile int kosh_exit = -2;
+
+static void kosh_thread(void *p) {
+	conio_printf("  **** KOSH, The KallistiOS Shell ****\n");
+	chdir("/");
+	while (!kosh_exit)
+		input_oneloop();
+	conio_printf("Kosh is done\n");
+	kosh_exit = -1;
+}
+
+int kosh_init() {
+	if (kosh_exit != -2)
+		return -1;
+
+	kosh_exit = 0;
+	builtins_init();
+	thd_create(kosh_thread, NULL);
+	return 0;
+}
+
+void kosh_shutdown() {
+	if (kosh_exit < 0) {
+		if (kosh_exit == -1)
+			builtins_shutdown();
+		kosh_exit = -2;
+		return;
+	}
+
+	kosh_exit = 1;
+	while (kosh_exit != -1)
+		thd_pass();
+	builtins_shutdown();
+	kosh_exit = -2;
+}
